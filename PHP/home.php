a8e2a57606cc92ec46f0b0bf750d7d7ed24685c4
SmitRami1 <122784110+SmitRami1@users.noreply.github.com>
2023-07-19 21:11:59 +0530

    Add files via upload

---
+++ PHP/home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | PC-WALA</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/home.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <img src="../Images/logo.gif" alt="Can't Load Image" id="logo">
+      <a class="navbar-brand" href="#">PC WALA</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#" id="hover">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#" id="hover">Category</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#" id="hover">Pre-Built</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#" id="hover">Contact Us</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="hover">
+          <a href="#"><img src="../Images/cart.png" alt="Can't Load Image" id="cart" id="hover"></a>
+        </form>
+      </div>
+    </div>
+  </nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</body>
+</html>
