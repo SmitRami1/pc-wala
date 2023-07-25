031204989a55691510bf48e9a28b923e75b2dbb4
rathod-jil <rathodjil1234@gmail.com>
2023-07-25 19:38:27 +0530

    dark theme and grid

--- PHP/home.php
+++ PHP/home.php
@@ -59,6 +59,96 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+<div class="gridrow">
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card card1">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros1">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card2">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros2">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card3">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros3">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card4">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..."id="pros4">
+    </div>
+  </div>
+</div>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card card1">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros1">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card2">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros2">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card3">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros3">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card4">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..."id="pros4">
+    </div>
+  </div>
+</div>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card card1">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros1">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card2">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros2">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card3">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros3">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card4">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..."id="pros4">
+    </div>
+  </div>
+</div>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card card1">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros1">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card2">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros2">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card3">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..." id="pros3">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card4">
+      <img src="../Images/i3.jpg" class="card-img-top" alt="..."id="pros4">
+    </div>
+  </div>
+</div>
+</div>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
