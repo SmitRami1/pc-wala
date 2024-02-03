ba37f71623720cba881ab5014f19462cedf9460d
adarshrami <ramiadarsh@outlook.com>
2024-02-03 17:44:14 +0530

    dsd

--- PHP/allproducts.php
+++ PHP/allproducts.php
@@ -59,58 +59,56 @@ if (isset($_SESSION["username"])) {
       </div>   
     </div>
   </nav>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pc-wala";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$category = $_GET['category'];
-$sql = "SELECT * FROM products WHERE category = '$category'";
-$result = $conn->query($sql);
-?>
-    <div id="products-container">
-        <?php
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-              echo ' <div class = "products">
-              <div class = "container">
-                  <div class = "product-items">
-                      <!-- single product -->
-                      <div class = "product">
-                          <div class = "product-content">
-                              <div class = "product-img">
-                                  <img src = "'. $row['image'] .'" alt = "product image">
-                              </div>
-                              <div class = "product-btns">
-                                  <button type = "button" class = "btn-cart"> add to cart
-                                      <span><i class = "fas fa-plus"></i></span>
-                                  </button>
-                                  <button type = "button" class = "btn-buy"> buy now
-                                      <span><i class = "fas fa-shopping-cart"></i></span>
-                                  </button>
-                              </div>
-                          </div>
-  
-                          <div class = "product-info">
-                              <a href = "#" class = "product-name">'. $row['name'] .'</a>
-                              <p class = "product-price">'. $row['price'] .'</p>
-                          </div>
-                      </div>
-  ';
-            }
-        } else {
-            echo '<p>No products found for the specified category.</p>';
-        }
-        ?>
-    </div>
+<div class="container">
+  <div class="row row-cols-auto">
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "pc-wala";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $category = $_GET['category'];
+    $sql = "SELECT * FROM products WHERE category = '$category'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo '<div class = "products">
+            <div class = "container">
+                <div class = "product-items">
+                  <div class="col">
+                    <div class = "product">
+                      <a href="#">
+                        <div class = "product-content">
+                            <div class = "product-img">
+                                <img src = "'.$row["image"].'" alt = "product image" id="pro-img">
+                            </div>
+                        </div>
 
-<?php
-$conn->close();
-?>
+                        <div class = "product-info">
+                            <div class = "product-info-top">
+                                <h2 class = "sm-title">'. $row["name"] .'</h2>
+                            </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>';
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
+
+  </div>
+</div>
+<footer class="footer">
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
