f423a003a2e727a08bd566fb900d093966467391
rathod-jil <139880289+rathod-jil@users.noreply.github.com>
2024-02-21 12:13:45 +0530

    Add files via upload

---
+++ PHP/product.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEMP PRODUCT</title>
+    <link rel="stylesheet" href="../CSS/products.css">
+</head>
+<body>
+    <main class="container">
+        <div class="left-column">
+          <img data-image="black" src="../Images/cabinet.png" alt="">
+        </div>
+        <div class="right-column">
+          <div class="product-description">
+            <span>Headphones</span>
+            <h1>Beats EP</h1>
+            <p>The preferred choice of a vast range of acclaimed DJs. Punchy, bass-focused sound and high isolation. Sturdy headband and on-ear cushions suitable for live performance</p>
+          </div>
+          <div class="product-price">
+            <span>148$</span>
+        </div>
+        <div class="buy-section">
+            <a href="#" class="cart-btn">Add to cart</a>
+            <a href="#" class="cart-btn">Buy Now</a>
+        </div>
+        </div>
+      </main>
+</body>
+</html>
