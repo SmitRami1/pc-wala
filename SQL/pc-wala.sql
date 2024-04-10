02248a1bb5f717cac2a2f4b8a9fef287c99d7636
adarshrami <139940850+adarshrami@users.noreply.github.com>
2024-04-10 11:54:31 +0530

    Add files via upload

--- SQL/pc-wala.sql
+++ SQL/pc-wala.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 20, 2024 at 06:40 AM
+-- Generation Time: Apr 10, 2024 at 06:21 AM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
@@ -50,22 +50,24 @@ INSERT INTO `cart` (`userid`, `productid`, `quantity`) VALUES
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `name` varchar(1000) NOT NULL,
-  `image` varchar(1000) NOT NULL
+  `image` varchar(1000) NOT NULL,
+  `card` int NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`name`, `image`) VALUES
-('processor', 'https://th.bing.com/th/id/R.de0d3c1bed03696aeedee32e911ac239?rik=Ql0ds7bRo%2bdLag&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0463%2f2539%2f9719%2fproducts%2f1_0facef5d-014d-4b3a-a907-972909111b09.png%3fv%3d1670578811&ehk=0J1p%2brJeoiikys6aThy8OW63JLwjkzeJXqGEEAtm5%2fA%3d&risl=&pid=ImgRaw&r=0'),
-('Graphics Card', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/graphics%20card.png?raw=true'),
-('Ram', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/ram.png?raw=true'),
-('Cabinet', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/cabinet.png?raw=true'),
-('Motherboard', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/motherboard.png?raw=true'),
-('SMPS', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/smps.png?raw=true'),
-('STORAGE', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/storage.png?raw=true'),
-('Monitor', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/monitor.png?raw=true');
+INSERT INTO `categories` (`name`, `image`, `card`) VALUES
+('processor', 'https://th.bing.com/th/id/R.de0d3c1bed03696aeedee32e911ac239?rik=Ql0ds7bRo%2bdLag&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0463%2f2539%2f9719%2fproducts%2f1_0facef5d-014d-4b3a-a907-972909111b09.png%3fv%3d1670578811&ehk=0J1p%2brJeoiikys6aThy8OW63JLwjkzeJXqGEEAtm5%2fA%3d&risl=&pid=ImgRaw&r=0', 1),
+('Graphics Card', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/graphics%20card.png?raw=true', 1),
+('Ram', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/ram.png?raw=true', 1),
+('Cabinet', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/cabinet.png?raw=true', 1),
+('Motherboard', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/motherboard.png?raw=true', 1),
+('SMPS', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/smps.png?raw=true', 1),
+('STORAGE', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/storage.png?raw=true', 1),
+('Monitor', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/monitor.png?raw=true', 1),
+('Pre-Built', 'https://github.com/SmitRami1/pc-wala/blob/main/Images/Pre-built_pc-removebg-preview%20(1).png?raw=true', 0);
 
 -- --------------------------------------------------------
 
